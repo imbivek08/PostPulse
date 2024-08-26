a16e9b9b81960a6791850b3a76abf0c37b6f0eaa
Bivek Yadav <118347286+imbivek08@users.noreply.github.com>
2024-08-26 22:30:37 +0530

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
           </h1>
           <div className="text-md text-gray-500 flex flex-col gap-6">
             <p>
-              Welcome to Bivek Blog! This blog was created by Sahand Ghavidel as
+              Welcome to Bivek Blog! This blog was created by Bivek Yadav as
               a personal project to share his thoughts and ideas with the world.
               Sahand is a passionate developer who loves to write about
               technology, coding, and everything in between.
